f825ca22c8762c5f622a159065a298104e2297ae
Aerius <Aerius@Aerius-PC>
2016-09-15 15:30:54 -0500

    completed - realistic Abstract version - Final

--- src/RealisticAbstractExample/HouseCat.java
+++ src/RealisticAbstractExample/HouseCat.java
@@ -23,7 +23,7 @@ public class HouseCat extends Feline{
     }
     
     @Override
-    public void useScratchingPost(){
+    public final void useScratchingPost(){
         if(this.isIsHungry()){
             System.out.println(this.getName() + " is using the scratching post!");
         }else{
@@ -32,7 +32,7 @@ public class HouseCat extends Feline{
     }
     
     @Override
-    public void eatFood(){
+    public final void eatFood(){
         if(this.isIsHungry()){
             System.out.println(this.getName() + " is hungry!");
         }else{
